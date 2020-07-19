a1946eccbfe5e504cfddff9701d3675205097aaa
gandalfmagic <gandalfmagic@libero.it>
2020-07-19 15:38:19 +0200

    Fix tests

--- test/system/error_10_error_log.sh
+++ test/system/error_10_error_log.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+trap 'echo "INT SIGNAL"; exit 255' INT
+
 i=1; while [ $i -le 5 ]; do sleep 0.01; i=$(( i + 1 )); done
 >&2 echo "write a line to stderr"
 i=1; while [ $i -le 5 ]; do sleep 0.01; i=$(( i + 1 )); done
